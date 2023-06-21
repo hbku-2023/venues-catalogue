c32e084ad4e8026436ef915c43c9b3f01aa4837c
Ahmed Matri <amatri@hbku.edu.qa>
2023-06-21 13:22:52 +0300

    upload custom_front.module

--- custom_front/src/Plugin/Block/SpotlightBlock.php
+++ custom_front/src/Plugin/Block/SpotlightBlock.php
@@ -1,27 +1,5 @@
 
 
-namespace Drupal\custom_front\Plugin\Block;
-
-use Drupal\Core\Block\BlockBase;
-use Drupal\node\Entity\Node;
-
-
-/**
- * Provides a block with a simple text.
- *
- * @Block(
- *   id = "spotlight_bock",
- *   admin_label = @Translation("Node spotlight"),
- *   category = "Custom"
- * )
- */
-class SpotlightBlock extends BlockBase {
-
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build() {
-
 		$node = \Drupal::routeMatch()->getParameter('node');
 
 		if ($node instanceof Node && !$node->field_media->isEmpty() ) {
@@ -30,12 +8,4 @@ class SpotlightBlock extends BlockBase {
 			];
 		}
 
-	}
 
-	/**
-	 * @return int
-	 */
-	public function getCacheMaxAge() {
-		return 0;
-	}
-}
